f275896b475ecd92f0b6f56ba3f2424b7b73bffe
snuggle@sprinkly.net <snuggle@sprinkly.net>
2018-04-29 17:45:04 +0100

    Added datetime & profiles!

--- messages/admin.py
+++ messages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Contact, Message
+from .models import Contact, Message, Profile
 
+admin.site.register(Profile)
 admin.site.register(Contact)
 admin.site.register(Message)
--- messages/models.py
+++ messages/models.py
@@ -2,19 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 import hashlib
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    avatar_image = models.CharField(max_length=100, blank=True)
+    def __str__(self):
+        return self.user.username
+
 class Contact(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE, default='Unknown', related_name='+')
     contact = models.OneToOneField(User, on_delete=models.CASCADE, default='Unknown', related_name='+')
     # The + means that this will not have a backwards relation for this attribute.
 
+    id = sorted([str(owner), str(contact)], key=str.lower)
+
     def __str__(self):
-        return f'{self.owner} ⯈ {self.contact}' # This represents this object as a string.
+        return f'{self.owner} ⬌ {self.contact}' # This represents this object as a string.
 
 class Message(models.Model):
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now=True, blank=True)
     message_text = models.CharField(max_length=1024)
     embed_image = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return hashlib.md5(str(self.message_text + str(self.contact)).encode('utf-8')).hexdigest()
+        return str(hashlib.md5(str(self.message_text + str(self.contact)).encode('utf-8')).hexdigest())[:10]
         # This returns md5(message_text + 'owner ⯈ contact')
