313c2c57019c8709073e258bef3f8cf4a06c438f
snuggle@sprinkly.net <snuggle@sprinkly.net>
2018-04-29 15:10:28 +0100

    Added travis.yml

--- messages/views.py
+++ messages/views.py
@@ -5,11 +5,10 @@ from django.http import HttpResponse
 
 def index(request):
     all_users = User.objects.all()
-    template = loader.get_template('messages/index.html')
     context = {
         'all_users': all_users,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'messages/index.html', context)
 
 def messageList(request, contact):
     return HttpResponse("<h2>Messages for Contact: " + str(contact) + "</h2>")
