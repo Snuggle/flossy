2d07ee37f4b8082d64aa8d58f82c6b0bedddf2c2
snuggle@sprinkly.net <snuggle@sprinkly.net>
2018-04-29 22:57:41 +0100

    Updated tests again!

--- messages/tests.py
+++ messages/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from django.test import Client
 
-testPaths = ['/', '/messages/', '/messages/1', '/messages/admin']
+testPaths = ['/', '/messages/', '/admin', '/settings', '/register']
 
 client = Client()
 for path in testPaths:
     response = client.get(path)
-    print(f"Path {path} exited with code {response.status_code}")
+    print(f"Path \"{path}\" exited with code: {response.status_code}")
